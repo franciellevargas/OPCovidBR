b41d068d448a7a1a9ccdfb9212e6617447c26de1
Francielle A. Vargas <franciellealvargas@gmail.com>
2020-04-15 13:34:08 -0300

    Add files via upload

--- API-twitter.py
+++ API-twitter.py
@@ -1,37 +1,26 @@
-# -*- coding: utf-8 -*- 
-
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
+# -*- coding: utf-8 -*-
+#/usr/local/lib/python3.6
 
 import tweepy
 import re
-
-# acessar a aba "Keys and Access Tokens"
-# passa o Consumer Key e o Consumer Secret
+#Autentication API Twitter number 12
 auth = tweepy.OAuthHandler('46pvnSwIVylfWepbPsP4433wL', 'xWDPHaUkk0ub93qj1DaYgJcO8QtkPUhNFIE7uBAvzbSVLLpLzR')
-
-# define o token de acesso
-# para criar basta clicar em "Create my access token"
-# passa o "Access Token" e o "Access Token Secret"
-auth.set_access_token('1952916806-9WbU9ROPLd4aVPprQZqWJhaW4RSXrBw4oK8A4Ow',
-		'gW5iuYPtrTxVhPmQxBemsKz6jCAOqbYx1fT0ewKHFyAkG')
-
-# cria um objeto api
+auth.set_access_token('1952916806-9WbU9ROPLd4aVPprQZqWJhaW4RSXrBw4oK8A4Ow', 'gW5iuYPtrTxVhPmQxBemsKz6jCAOqbYx1fT0ewKHFyAkG')
 api = tweepy.API(auth)
+
 search_term = 'coronavírus'
 search_term2 = 'codiv-19'
 
-c=tweepy.Cursor(api.search,
-            q="{}+OR+{}".format(search_term,search_term2),
-            rpp=1000,
-            lang='pt',
-            include_entities=True)
 
+tweets = api.search(q="coronavírus", lang="pt", count=2000, tweet_mode='extended')
+i = 37
 data = {}
-i = 1
-for tweet in c.items():
-    data['text'] = tweet.text
-    print(i, ":", data)
-    i += 1
-time.sleep(1)
+for tweet in tweets:
+    try:
+    	data['text'] = tweet.retweeted_status.full_text
+    	print(i, data)
+    	i += 1
+    except AttributeError:  # Not a Retweet
+    	data['text'] = tweet.full_text
+    	print(i, data)
+    	i += 1
